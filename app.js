1af0b1439b940dec227e15ededf7a77a6ba8204f
kuldeep <kldpsngh700@gmail.com>
2023-02-02 20:48:52 +0530

    Final rest api commit

--- app.js
+++ app.js
@@ -1,18 +1,78 @@
-const express = require('express');
-const feedRoute = require('./routes/feed');
-const bodyParser = require('body-parser');
+const path = require('path');
+const express = require("express");
+const feedRoute = require("./routes/feed");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require('multer');
+const authRoute = require('./routes/auth');
+
+const { v4: uuidv4 } = require('uuid');
+
+const dotenv = require('dotenv');
+dotenv.config()
 
 const app = express();
 
+
+const fileStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'images')
+    },
+    filename: function (req, file, cb) {
+      //const uniqueSuffix = Date.now() + '-' + file.originalname)
+     //cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
+    }
+  });
+
+  const fileFilter = (req, file, cb) => {
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin',"*");
-    res.setHeader('Access-Control-Allow-Method',"OPTIONS, GET, POST, PUT, PATCH, DELETE");
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Autherization');
-    next();
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  );
+app.use('/images',express.static(path.join(__dirname,'images')));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed',feedRoute);
+app.use("/feed", feedRoute);
+app.use('/auth', authRoute);
 
-app.listen(9090);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message });
+  });
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(result=>{
+    app.listen(9090);
+  })
+  .catch(err=>{
+    console.log(err);
+  });
+
+
